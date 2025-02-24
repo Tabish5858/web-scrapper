a72a0c8efe903f7ba75f2abeae9e3f539cc4dd63
Tabish <tabishbinishfaq1122.com>
2025-02-24 17:38:29 +0500

    update WSGI configuration to change project path and create uploads directory

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,18 @@
 import sys
 import os
 
-# Add your project directory to Python path
-path = '/home/tabish/flask-web-scraper'
-if path not in sys.path:
-    sys.path.append(path)
+# Add project directory to Python path
+project_path = '/home/tabish/web-scrapper'
+if project_path not in sys.path:
+    sys.path.append(project_path)
 
-from src.app import app as application
+# Add src directory to Python path
+src_path = os.path.join(project_path, 'src')
+if src_path not in sys.path:
+    sys.path.append(src_path)
+
+# Create uploads directory
+uploads_path = os.path.join(project_path, 'uploads')
+os.makedirs(uploads_path, exist_ok=True)
+
+from app import app as application
